f0c6b8868e8cec19b0acbfdc64ea45bbfd59b02f
Oleg Kharchuk <olegease@gmail.com>
2018-11-20 23:55:28 +0200

    etc test duration calls: fixes for heavy function calls, need rework for light function calls sleep stragedy, now it's eat to much resources of cpu

--- etc/test.cpp
+++ etc/test.cpp
@@ -15,26 +15,44 @@ struct Call
     Call(f* function_to_call, int every_milliseconds = 1) : func(function_to_call), ms(every_milliseconds) { }
 };
 
-template< typename T, int sz = 200 >
+template< typename T >
 struct array_nano_duration
 {
     using clock = std::chrono::high_resolution_clock;
     int index;
-    std::array< T, sz > data;
+    std::array< T, 2000 > data;
     array_nano_duration() : index(0)
     {
         for (int key = 0; key < data.size(); ++key) {
             auto start = clock::now();
-            std::this_thread::sleep_for(std::chrono::nanoseconds(key * 5'000));
+            std::this_thread::sleep_for(std::chrono::nanoseconds(nano_progression(key)));
             data[key] = std::chrono::duration_cast<std::chrono::nanoseconds>(clock::now() - start).count();
         }
+    }
+
+    T nano_progression(int key)
+    {
+        static constexpr T values[] = {
+            0, 1, 5, 10, 50, 100, 200, 500, 750, 1'000, 5'000, 7'500, 10'000, 15'000, 20'000, 50'000, 100'000, 250'000, 500'000, 750'000
+        };
+        return 500 * key;
+    }
+
+    void cout()
+    {
+        int key = 0;
+        std::cout << "Data nano duration key: value{" << std::endl;
+        for (auto v : data) {
+            std::cout << key++ << ": " << v << std::endl;
+        }
+        std::cout << "}" << std::endl;
     }
 
     T sleep_for(T initial_sleep)
     {
         T best = 0;
-        double best_diff = std::abs(data[0] - initial_sleep);
-        double diff;
+        auto best_diff = std::abs(data[0] - initial_sleep);
+        decltype(best_diff) diff;
         for (auto key = 1; key < data.size(); ++key) {
             if (initial_sleep - data[key] < 0) continue;
             diff = initial_sleep - data[key];
@@ -43,7 +61,7 @@ struct array_nano_duration
             best = key;
         }
         index = best;
-        return best * 5'000;
+        return nano_progression(best);
     }
     void update_index(long long value)
     {
@@ -53,7 +71,8 @@ struct array_nano_duration
 
 void call()
 {
-    std::cout << "call" << std::endl;
+    std::this_thread::sleep_for(std::chrono::nanoseconds(5));
+    //std::cout << " ";
 }
 
 void test(Call::f* func = call)
@@ -69,14 +88,15 @@ void test(Call::f* func = call)
         auto rounding_cycles = 0;
         // local while, run function 1000 times and try sleep when it too fast
         while (true) {
-            if (ticks == 1000 || global_func_duration > 1'000'000'000) break;
+            if (ticks == 1000 || global_func_duration > 975'000'000) break;
             auto func_start = clock::now();
             func();
             auto func_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(clock::now() - func_start).count();
             global_func_duration += func_duration;
             ticks++;
-            auto expected_duration = ticks * 1'000'000;
+            auto expected_duration = (ticks * 1'000'000) - (ticks * 25'000);
             while (expected_duration > global_func_duration) {
+                // TODO find best way to sleep, now it's too heavy
                 auto sleep_start = clock::now();
                 rounding_cycles++;
                 std::this_thread::sleep_for(
@@ -96,7 +116,7 @@ void test(Call::f* func = call)
         std::cout << "ticks: " << ticks << std::endl;
         std::cout << "avg rounding cycles: " << rounding_cycles / 1000 << std::endl;
         if (global_duration < 975'000'000 || global_duration > 1'025'000'000) throw std::runtime_error("duration check failed");
-        break;
+        //break;
     }
 }
 
